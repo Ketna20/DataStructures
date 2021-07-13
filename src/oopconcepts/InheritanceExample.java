69ee416b57ac57cb175810dbf1bad9e513525310
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2021-07-12 20:34:15 -0700

    Inheritance Example code is added.

---
+++ src/oopconcepts/InheritanceExample.java
@@ -0,0 +1,12 @@
+package oopconcepts;
+/*
+ * ketnakhalasi created on 7/12/21
+ * */
+
+public class InheritanceExample {
+
+    public static void main(String[] args) {
+        Programmer p = new Programmer("KK", "T1");
+        System.out.println(p.getName() + " is assigned to team " + p.getTeam());
+    }
+}
---
+++ src/oopconcepts/Programmer.java
@@ -0,0 +1,21 @@
+package oopconcepts;
+/*
+ * ketnakhalasi created on 7/12/21
+ * */
+
+public class Programmer extends Employee {
+    private String team;
+
+    public Programmer(String name, String team) {
+        super(name);
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+}
