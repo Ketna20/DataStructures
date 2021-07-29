621450bfd92ab657beb9e7969df99657bec1b4ba
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2021-07-29 15:50:32 -0700

    Repeated String HackerRank coding Example is added.

--- src/intervwPrep/RepeatedStrings.java
+++ src/intervwPrep/RepeatedStrings.java
@@ -8,20 +8,25 @@ public class RepeatedStrings {
     public static void main(String[] args) {
         String s = "aba";
         long n = 10;
-        long len = n / s.length();
-        System.out.println("len: " + len);
         System.out.println(repeatedString(s, n));
     }
-
-    static long repeatedString(String s, long n) {
-        char[] c = s.toCharArray();
+    public static long repeatedString(String s, long n) {
+        long numOfReps = n/s.length();
+        long remainder = n%s.length();
         long repeated = 0;
-        int strLength = c.length;
-        for(char a : c) {
-            if (a == 'a') {
+        for (int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == 'a'){
+                repeated++;
+            }
+        }
+        repeated = repeated * numOfReps;
+        for (int i=0; i<remainder; i++) {
+            if(s.charAt(i) == 'a') {
                 repeated++;
             }
         }
         return repeated;
     }
+
+
 }
