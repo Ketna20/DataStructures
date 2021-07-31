b3ff5c62694118760363cae61c86ff9ab009b672
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2021-07-30 21:27:19 -0700

    A simple Binary Tree coding Example is added.

---
+++ src/trees/BinaryTree.java
@@ -0,0 +1,15 @@
+package trees;
+/*
+ * ketnakhalasi created on 7/30/21
+ * */
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree(int key) {
+        root = new Node(key);
+    }
+    public BinaryTree() {
+        root = null;
+    }
+}
---
+++ src/trees/Node.java
@@ -0,0 +1,40 @@
+package trees;
+/*
+ * ketnakhalasi created on 7/30/21
+ * */
+
+public class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
