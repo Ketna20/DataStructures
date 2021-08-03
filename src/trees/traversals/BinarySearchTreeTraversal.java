cad99897cd747634f801671667e31c3609f2acc5
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2021-08-03 11:51:18 -0700

    implemented contains method.

--- src/trees/traversals/BinarySearchTreeTraversal.java
+++ src/trees/traversals/BinarySearchTreeTraversal.java
@@ -7,20 +7,44 @@ public class BinarySearchTreeTraversal<T extends Comparable<T>> {
     private int nodeCount;
     private Node root = null;
 
-    private boolean contains(Node current, T element) {
-        if(root == null || element == null) {
+    public boolean insert(T element) {
+        if(element == null) {
             return false;
         }
-        if(element.compareTo((T) current.element) == 0) {
-            return true;
+        if(contains(element)) {
+            return false;
         }
-        return element.compareTo((T) current.element) < 0 ?
-                contains(current.left, element) :
-                contains(current.right, element);
+        root = insert(root, element);
+        nodeCount++;
+        return true;
+    }
+
+    private Node insert(Node current, T element) {
+        if(current == null) {
+            return new Node(null, null, element);
+        }
+        if(element.compareTo((T) current.element) < 0) {
+            current.left = insert(current.left, element);
+        } else {
+            current.right = insert(current, element);
+        }
+        return current;
     }
 
     public boolean contains(T element) {
         return contains(root, element);
+    }
 
+    private boolean contains(Node current, T element) {
+        if (root == null || element == null) {
+            return false;
+        }
+        if(element.compareTo((T) current.element) == 0){
+            return true;
+        }
+
+        return element.compareTo((T) current.element) < 0
+               ? contains(current.left, element)
+               : contains(current.right, element);
     }
 }
