ec6b402b10b7f8f2a3f6cbb083a23e67d00867d6
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2021-08-03 18:00:35 -0700

    Searching a key utility method of Binary Search Tree is added.

--- src/intervwPrep/BinarySearchTree.java
+++ src/intervwPrep/BinarySearchTree.java
@@ -45,4 +45,26 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return true;
     }
 
+    /* Hacker Rank Solution
+    boolean checkBST(Node root) {
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    boolean checkBST(Node node, int min, int max) {
+        if (node == null) return true;
+        return  min < node.data && node.data < max &&
+            checkBST(node.left, min, node.data) &&
+            checkBST(node.right, node.data, max);
+    }
+     */
+    //search an element in BST
+    public Node search(Node root, int element) {
+        if(root == null || root.element == element) {
+            return root;
+        }
+        if(element < root.element) {
+            return search(root.left, element);
+        }
+
+        return search(root.right, element);
+    }
 }
