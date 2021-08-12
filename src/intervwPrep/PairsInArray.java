32e7f76e4d880b630a8abae707d5360400199cfe
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2021-08-11 23:20:34 -0700

    Find pairs in array whose sum equals K.

---
+++ src/intervwPrep/PairsInArray.java
@@ -0,0 +1,10 @@
+package intervwPrep;
+/*
+ * ketnakhalasi created on 8/11/21
+ * */
+
+public final class PairsInArray {
+    private PairsInArray() {
+        throw new AssertionError("Cannot be instantiated.");
+    }
+}
