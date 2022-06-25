5132f436bb97b0ca55152dd9fef84ded64303a83
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2022-06-25 16:46:26 -0700

    IK Problems -CircularQueue implementation with resizing Queue.

--- src/ik/learn/queue/CircularQueue.java
+++ src/ik/learn/queue/CircularQueue.java
@@ -16,20 +16,48 @@ public class CircularQueue {
     }
 
     public boolean enQueue(int value){
-        if(isFull()) return false;
-
+//        if(isFull()) return false;
+        if(isFull()) {
+            resize(2*size);
+        }
         count++;
         end = (end+1) % size;
+        System.out.println("end: " + end);
         arr[end] = value;
         return true;
     }
 
+    // resize the array and copy elements in new array with head to tail order
+    // means example - 4 0 0 1 2 3 old array - size = 6
+    // new array after copying elements - 1 2 3 4 0 0 0 0 0 0 0 0 - size = 12
+    public void resize(int newSize) {
+        int[] newArr = new int[newSize];
+        int nextIndex = 0;
+
+        //copy elements from old array to new Array
+        while(front != end) {
+            newArr[nextIndex] = arr[front];
+            nextIndex++;
+            front = (front+1) % size;
+        }
+        newArr[nextIndex] = arr[front];
+        arr = newArr;
+        front = 0;
+        end = nextIndex - 1;
+        size = newSize;
+    }
+
     public boolean deQueue(){
         if(isEmpty()) return false;
+        if(count == 1){
+            arr[front] = -1;
 
+        }
         front++;
         front=front%size;
+        System.out.println("front: " + arr[front]);
         count--;
+        arr[front] = -1;
         return true;
     }
 
