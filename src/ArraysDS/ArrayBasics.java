bc5135fbcc1bafc4b061d86d109641527188037c
ketna20 <ketna.kumar@gmail.com>
2024-08-30 19:44:52 -0700

    ArrayBasic operations findElement and deleteElement implemented.

--- src/ArraysDS/ArrayBasics.java
+++ src/ArraysDS/ArrayBasics.java
@@ -1,5 +1,6 @@
 package ArraysDS;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class ArrayBasics {
@@ -18,14 +19,33 @@ public class ArrayBasics {
         arrayList.add(4);
 
         int[] newArr = insertElement(arr, arr.length, 7, 1);
+        System.out.println("New Array after insert Element");
         traverseArray(newArr);
 
+        boolean is7Present = isElementExist(newArr, 7);
+        System.out.println();
+        System.out.println("7 is exist in newArray: " + is7Present);
+
+        boolean is9Present = isElementExist(newArr, 9);
+        System.out.println("9 is exist in newArray: " + is9Present);
+
+        int position =  findElementPosition(newArr, 3);
+        System.out.println("position of 3 in newArr:  " + position);
+
+
+        System.out.println("Delete 2:");
+        int newArrSize = deleteElement(newArr, 2);
+        System.out.println();
+        System.out.println("newArrSize after deletion " + newArrSize);
     }
 
     static void traverseArray(int arr[]) {
         // Traversing over arr[]
         for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i]);
+            if(i < arr.length-1) {
+                System.out.print(", ");
+            }
         }
     }
 
@@ -45,5 +65,50 @@ public class ArrayBasics {
 
         return newArr;
     }
+
+    static boolean isElementExist(int arr[], int k) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // find element in array
+    static int findElementPosition(int arr[], int key) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // delete element from an array
+    static int deleteElement(int arr[], int key) {
+        int position = findElementPosition(arr, key);
+        int n = arr.length;
+        if(position == -1) {
+            System.out.println("Element is not found.");
+            return n;
+        }
+        for(int i=position; i<n-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        int[] newArr = copyArr(arr);
+        traverseArray(newArr);
+        return newArr.length;
+    }
+
+    static int[] copyArr(int oldArr[]) {
+        int length = oldArr.length;
+        int newArr[] = new int[length-1];
+        for(int i=0; i<length-1; i++) {
+            newArr[i] = oldArr[i];
+        }
+        return newArr;
+    }
+
 
 }
