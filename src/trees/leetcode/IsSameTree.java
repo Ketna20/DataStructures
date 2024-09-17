eacee3fd7e07bd77f0310ab8776784196e4ec540
ketna20 <ketna.kumar@gmail.com>
2024-09-16 23:22:32 -0700

    IsSameTree implemented.

---
+++ src/trees/leetcode/IsSameTree.java
@@ -0,0 +1,15 @@
+package trees.leetcode;
+
+/**
+ * Leetcode 100: isSameTree
+ */
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+            return true;
+        if(p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
