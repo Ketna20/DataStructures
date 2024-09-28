a81bb23270bf623686e99288f4ed175cc61cd23a
ketna20 <ketna.kumar@gmail.com>
2024-09-28 15:55:21 -0700

    Fibonacci optimized algorithm implemented.

--- src/bigOAnalysis/Fibonacci.java
+++ src/bigOAnalysis/Fibonacci.java
@@ -20,4 +20,18 @@ public class Fibonacci {
         return fibonacci(k-1) + fibonacci(k-2);
     }
 
+    //time complexity O(n) and Space O(1)
+    public int fib(int n) {
+        if (n <= 1) return n;
+
+        int prev1 = 0;
+        int prev2 = 1;
+        int current = 0;
+        for(int i = 2; i<=n; i++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return current;
+    }
 }
