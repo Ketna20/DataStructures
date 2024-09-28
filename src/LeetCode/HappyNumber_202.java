aa9055ac8c6dc8d3774921e442714332c18556cf
ketna20 <ketna.kumar@gmail.com>
2024-09-27 21:34:26 -0700

    HappyNumber_202 implemented.

--- src/LeetCode/HappyNumber_202.java
+++ src/LeetCode/HappyNumber_202.java
@@ -15,10 +15,10 @@ import java.util.Set;
  * Input: n = 19
  * Output: true
  * Explanation:
- * 12 + 92 = 82
- * 82 + 22 = 68
- * 62 + 82 = 100
- * 12 + 02 + 02 = 1
+ * (1*1) + (9*9) = 82
+ * 8*8 + 2*8 = 68
+ * 6*6 + 8*8 = 100
+ * 1*1 + 0*0 + 0*0 = 1
  */
 public class HappyNumber_202 {
 
